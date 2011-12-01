58aef947676d07671c92e8fa0e01a4f01933e891
giorgio gemignani <giorgio.gemignani@gmail.com>
2011-11-30 19:22:23 -0500

    ultimo

--- MonteCarloMethods/ParticleFilter/EsempioIS.m
+++ MonteCarloMethods/ParticleFilter/EsempioIS.m
@@ -1,4 +1,4 @@
-%% Applicazione  di MonteCarlo Approximation e del IS ( importance Sampling )
+%% Applicazione  di MonteCarlo Approximation e IR ed IS ( importance Sampling Resampling)
 %  per la stima approssimata del Valore Atteso:
 %              I                     1            i  
 %  E[ f(x) ] = I   f(x) p(x) dx ~   ---  sum( f( x ) )
@@ -26,12 +26,16 @@
 %
 %    i                                 i      i
 % - w : PESI delle PARTICELLE  --> p( x )/g( x )
-clear all;
+%
+%
+% Incrementando il Numero di Particelle e' possibile osservare come
+% si riduce l'errore di approssimazione ( teoricamente O(N^-1/2 ))
+clear all; close all
 mu = 0.8; %
 sigma = sqrt(1.5); %
 k=1.65; %
 c=2; %
-num_iterations = 100; %
+num_iterations = 10; %
 % TARGET DISTRIBUTION
 p = @(x) x.^(k-1).*exp(-x.^2/2); 
 
@@ -51,51 +55,64 @@ figure(1);
 plot(x,p(x),'b');hold on
 plot(x,q(x),'r');
 plot(x,p(x).*f(x),'g'); 
-legend('p(x)','q(x)','p(x)*f(x)')
+legend('p(x)','q(x)','f(x)*p(x)')
 
+
+samp_size = 2500;
 %SCHEMA IMPORTANCE SAMPLING  eseguito per num_iterations Simulazioni
-for iter=1:num_iterations
-    samp_size = 1000; 
+for t=1:num_iterations
+     
     % CAMPIONO da q(x) : X --> Vettore dei samp_size Samples
-    X = normrnd(mu,sigma,[samp_size 1]);
+    Samples = normrnd(mu,sigma,[samp_size 1]);
      
     %Calcolo I Pesi
-    for i=1:length(X)
+    j=0;
+    for i=1:length(Samples)
         %Considero solo i sample Positivi perche' l'obbiettivo e
         %approssimare l'integrale in [0,5]
-        if X(i)>=0
-            %Calcolo I PESI
-            W(i) = p(X(i))/q(X(i));
-            %Calcolo i termini da Sommare nell' approssimazione
-            I(i) = W(i)*f(X(i));
-        else
-            %Assegno peso nullo e termine nell 'integrale nullo in modo da
-            %non inficiare l'approssimazione
-            W(i)=0;
-            I(i)=0;
-            %Aggiorna il sample size: numero di samples utilizzati per
-            %approssimare l'integrale
-            samp_size = samp_size - 1;
+        if Samples(i) >=0
+            j = j + 1;
+            X(j)=Samples(i);
+            
         end
     end
+    %Calcolo I PESI
+    W=p(X )./q(X );
     
-    %Calcola l'integrale Approssimato (Montecarlo)
-    I_hat = sum(I)/sum(W);
+    %Calcolo i termini da Sommare nell' approssimazione
+    I=W.*f(X);
+    %Calcola l'integrale Approssimato: Montecarlo sui campioni Estratti 
+    I_hat_is(t) = sum(I)/sum(W);
     
-    [tmp tmp2 non_zero_weights] = find(W);
-    %Varianza dei Samples generati
-    variance = var(non_zero_weights);
-    eff_samp_size = samp_size/(1 + variance);
-    %Salvo i seguenti risultati ad ogni Simulazione
-    % - Integrale Approssimato
-    % - Varianza dei Samples generati
-    % - Numero di Campioni Generati
-    % - effective sample size
-    format short g
-    results(iter,:) = [I_hat variance samp_size eff_samp_size];
+    %     figure(2);
+    %     clf;
+    %     plot(X,W,'ro');hold on
+    
+    %% RESAMPLING con Campionamento Inverso Discreto se le particelle 
+    %  generate sono poche
+    % 
+    % controllo se sia necessario ricampionare
+    % neff=1/(W*W.');
+    % if(neff < samp_size/10)
+        
+        W=W/sum(W);
+        Index=CampionamentoInversoDiscreto(W, samp_size)+1;
+        Xres=X(Index);
+        %Calcolo I PESI
+        Wres=ones(1,samp_size)/samp_size;
+        %Calcolo i termini da Sommare nell' approssimazione
+        I=Wres.*f(Xres);
+        %Calcola l'integrale Approssimato: Montecarlo sui campioni Resampled 
+        I_hat_ir(t) = sum(I)/sum(Wres);
+
+        
+    %    plot(Xres,Wres,'bo')
+    %    legend(['IS-I=' num2str(I_hat_is)],['IR-I=' num2str(I_hat_ir)])
+      
+    %end
+ 
+    
  
  
 end
-% Calcolo i Valori MEdi e Varianza dei risultati ottenuti
-totals = [mean(results(:,1)) mean(results(:,2)) mean(results(:,3)) mean(results(:,4));...
-          var(results(:,1)) var(results(:,2)) var(results(:,3)) var(results(:,4))]
+disp([I_hat_is ; I_hat_ir])
