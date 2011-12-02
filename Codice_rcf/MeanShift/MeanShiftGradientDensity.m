a3550f644f84c4d52d80cb2c3d70c67710baf27f
giorgio gemignani <gemiggio@umich.edu>
2011-12-02 14:40:44 +0100

    Mean Shift 2-12-2011

--- Codice_rcf/MeanShift/MeanShiftGradientDensity.m
+++ Codice_rcf/MeanShift/MeanShiftGradientDensity.m
@@ -10,45 +10,57 @@ SIGMA2 = [10 -0.6;
           -0.6 8 ];
 Npoints=100;
 Xi =[ mvnrnd(mu1,SIGMA1,Npoints) ;mvnrnd(mu2,SIGMA2,Npoints)];
+Xi=Xi.';
 Npoints=2*Npoints;
 
 %Disegno  il punto iniziale
-plot(Xi(:,1),Xi(:,2),'+');hold on;
+plot(Xi(1,:),Xi(2,:),'+');hold on;
 
 %Disegno le Mode della distribuzione
 plot(mu1(1),mu1(2),'*r');
 plot(mu2(1),mu2(2),'*r');
 
-%syms x u s;
+syms x y;
+Z=[x;y]
 %f=exp(-x^2/2);
 %df=diff(f,'x')
 
-g=@(x)-0.5*(exp(-(x^2)));
-X=[30 20];
+g=@(x)0.5*(exp(-(x^2)));
+X=[30;20];
 plot(X(1),X(2),'or'); hold on
-h=0.9;
+h=6;
 
 %Eseguo T iterazioni per vedere dove va il Mean shift
 T=50;
 t=2;
  
-Xp=zeros(T,2);
-Xp(1,:)=X;
+Xp=zeros(2,T);
+Xp(:,1)=X;
+plot(Xp(1,1),Xp(2,1),'og');
 while t< T
    %Calcolo il Mean Shift Vector
-   X=repmat(X,Npoints,1);
-   Y=(X-Xi)/h^2;
-   Num=0;
-   DeNum=0;
+   Circ= (Z-X).'*(Z-X)-h^2;
+   ezplot(Circ,[-10^2 10^2])
+   %Cerco i punti che cadono nell'IperSfera Centrata in X
+   Sx=[];
+   Nx=0; 
    for i=1:Npoints
-        y=norm(Y(i,:),2); %*Y(i,:).'
-        Num=Num+Xi(i,:)*g(y);
-        DeNum=DeNum+g(y);
+        if (X-Xi(:,i)).'*(X-Xi(:,i))< h^2
+            Sx=[Sx Xi(:,i)];
+            Nx=Nx+1;
+        end
    end
-   MeanShift=Num/DeNum; 
-   Xp(t,:)=Xp(t-1,:)-MeanShift;
-   X=Xp(t,:);
+   
+   X=repmat(X,1,Nx);
+   %Il mean shift è un Media Locale
+   MeanShift=sum(Sx-X,2)/Nx;
+   %Fhu=Nx/(N*h^2*pi);
+   
+   Xp(:,t)=MeanShift+Xp(:,t-1);
+   X=Xp(:,t);
+   plot(Xp(1,1:t),Xp(2,1:t),'og-');
    t=t+1;
 end
 
-plot(Xp(:,1),Xp(:,2),'og-')
+%plot(Xp(:,1),Xp(:,2),'og-');
+%plot(Xp(1,1),Xp(1,2),'+r')
