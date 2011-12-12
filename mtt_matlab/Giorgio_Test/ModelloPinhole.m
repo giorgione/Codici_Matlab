7c77d571efed484803653388fd07c31cfc48f6d9
giorgio gemignani <gemiggio@umich.edu>
2011-12-12 09:27:33 +0100

    modello pinhole funzionante

--- mtt_matlab/Giorgio_Test/ModelloPinhole.m
+++ mtt_matlab/Giorgio_Test/ModelloPinhole.m
@@ -19,7 +19,7 @@ ylabel('Y')
 zlabel('Z') 
 title('WORLD REFERENCE SYSTEM')
 %Fisso la distanza focale a 10
-f=10;
+f=1;
 
 %%    ROTOTRASLAZIONE che lega sistema di riferimento CAMERA e MONDO
 %
@@ -43,6 +43,8 @@ Zw=[30;20;20]
 plot3(Zw(1),Zw(2),Zw(3),'or','MarkerFaceColor','b','MarkerEdgeColor','b','MarkerSize',5);
 %Lina passante per il CO
 Prj=[CO Zw];
+line(Prj(1,:),Prj(2,:),Prj(3,:),'Color','r','LineStyle','-')
+Prj=[[0;0;0] CO];
 line(Prj(1,:),Prj(2,:),Prj(3,:),'Color','r','LineStyle','-')
 
 %punto Zw sul piano di terra
@@ -74,13 +76,15 @@ N=-T;
 % Calcolo i generatori
 N=N/norm(N);
 Ac=null(N.');
-%Matrice di Rotazone Per allineare Mondo a Camera
-R=f*[Ac N];
+%Matrice di Rotazone Per allineare Mondo a Camera: i vettori colonna sono
+%normalizzati
+Rw2c=[Ac(:,1) -Ac(:,2) N];
 % Vettore3D(R(1,1),R(2,1),R(3,1),colorX,1) %X in red
 % Vettore3D(R(1,2),R(2,2),R(3,2),colorY,1) %Y in verde  
 % Vettore3D(R(1,3),R(2,3),R(3,3),colorZ,1) %Z in blue
 
-Ac=f*[Ac(:,1) -Ac(:,2) N];
+%Vettori generatori del Piano Immagine in Coordinate MONDO
+Ac=10*Rw2c;
 % Sistema di RIFERIMENTO CAMERA centrato in T
 % Ac--> Assi Camera di lunghezza f: sono VETTORI APPLICATI nell'origine
 %       Sommando T ottengo i punti sugli ASSI a distanza f dal Centro
@@ -90,12 +94,13 @@ Ac=f*[Ac(:,1) -Ac(:,2) N];
 Vettore3D_Applicato(T,Ac(1,1),Ac(2,1),Ac(3,1),colorX)
 Vettore3D_Applicato(T,Ac(1,2),Ac(2,2),Ac(3,2),colorY)
 Vettore3D_Applicato(T,Ac(1,3),Ac(2,3),Ac(3,3),colorZ)
- 
+
 %Considero i Punti in CORDINATE MONDO sugli assi del Piano Focale
 Ac1=Ac+repmat(T,1,3);
 plot3(Ac1(1,1),Ac1(2,1),Ac1(3,1),'or','MarkerFaceColor','r','MarkerEdgeColor','r','MarkerSize',5);
 plot3(Ac1(1,2),Ac1(2,2),Ac1(3,2),'og','MarkerFaceColor','g','MarkerEdgeColor','g','MarkerSize',5);
 plot3(Ac1(1,3),Ac1(2,3),Ac1(3,3),'ob','MarkerFaceColor','b','MarkerEdgeColor','b','MarkerSize',5);
+
 %Disegno il Piano Immagine-Camera
 w=10;
 h=8;
@@ -125,21 +130,24 @@ line(Prj(1,:),Prj(2,:),Prj(3,:),'Color','b','LineStyle','-')
 %                    -1
 %   Zc=R*Zw+T   --> R  (Zc-T)=Zw
 %
-Rw2c=Ac;
-
 %ROTAZIONE CAMERA TO MONDO
 Rc2w=inv(Rw2c);
-
+ku=10;
+kv=10;
 %MATRICE INTRINSECI
-K=[f 0 w/2
-   0 f h/2
-   0 0  1];
+K=[f*ku  0      w
+   0    f*kv    h
+   0     0      1];
 
+%Proietto nel Piano Immagine
+P=K*(Rw2c*Zw+T)
+%Divido per z
+P2D=P/P(3)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   DISEGNO IL MODELLO IN COORDINATE CAMERA (camera centrata nello 0)
 %
-% Per fare questo devo applicare una ROTOTRASLAZIONE al sistema centrato
-% nella camera:
+% Per fare questo devo applicare la trasformazione INVERSA alla CAMERA
+% ovvero allineare la camera a l MONDO
 %
 %     1)   Sottrarre il Vettore T per centrare il Centro Ottico nell'origine
 %          lavorando sui vettori del piano non mi serve
@@ -149,33 +157,41 @@ K=[f 0 w/2
 %               - l' asse Z sia concidente con N
 %               - l' asse X sia l'asse Orizzontale del piano immagine
 %                - l' asse Y sia l'asse Verticale del piano immagine
-%        x   y  z
-Assi = [ 1   0  0;
-         0   0  1;
-         0  -1  0];
+%            x   y  z
+Assi =     [ 1   0  0;
+             0   0  1;
+             0  -1  0];
 %Matrice di Rotazione che mi consente di disegnare la camera centrata
 %nell'origine
-R=Assi*inv(Ac);
+R=Assi;
 %Verifico che R ruota gli assi della camera e li dispone come richiesto
 % dal modello pinhole
-Ac=R*Ac;
-Vettore3D(Ac(1,1),Ac(2,1),Ac(3,1),colorX,1) %X in red
-Vettore3D(Ac(1,2),Ac(2,2),Ac(3,2),colorY,1) %Y in verde  
-Vettore3D(Ac(1,3),Ac(2,3),Ac(3,3),colorZ,1) %Z in blue
+% figure(3)
+% Vettore3D(Ac(1,1),Ac(2,1),Ac(3,1),colorX,3) %X in red
+% Vettore3D(Ac(1,2),Ac(2,2),Ac(3,2),colorY,3) %Y in verde  
+% Vettore3D(Ac(1,3),Ac(2,3),Ac(3,3),colorZ,3) %Z in blue
+% Ac=R*Ac;
+% Vettore3D(Ac(1,1),Ac(2,1),Ac(3,1),colorX,3) %X in red
+% Vettore3D(Ac(1,2),Ac(2,2),Ac(3,2),colorY,3) %Y in verde  
+% Vettore3D(Ac(1,3),Ac(2,3),Ac(3,3),colorZ,3) %Z in blue
 
     
 %Assi Camera allineati con il sistema di Riferimento
 Ac2=5*Assi;
     
-figure(2);hold on
+figure(2);
+hold on
 title('CAMERA REFERENCE SYSTEM')
-%L'origine del sistema di Riferimento Mondo si trovera ad una distanza T
-%dal Centro Ottico lungo l'asse ottico
+%L'origine del sistema di Riferimento Mondo si trovera ad una distanza
+%norm(T) dal Centro Ottico lungo l'asse ottico
 dis=norm(T)
 Ow=[0;dis;0]
+
+Ow1=R*(Rc2w*([0;0;0]-T));
 plot3(Ow(1),Ow(2),Ow(3),'oy','MarkerFaceColor','y','MarkerEdgeColor','y','MarkerSize',5);
 %Centro Ottico
 Co1=[0;0;0];
+Co2=R*(Rc2w*(T-T));
 plot3(Co1(1),Co1(2),Co1(3),'oy','MarkerFaceColor','y','MarkerEdgeColor','y','MarkerSize',5);
 %Centro Immagine
 CI1=Co1+[0;f;0];
@@ -207,11 +223,8 @@ line(Prj(1,:),Prj(2,:),Prj(3,:),'Color','b','LineStyle','-')
 
 %Considero gli Assi del sistema MONDO trasformati nel nuovo sistema di
 %Riferimento
-%Ac1=Rc2w*(Aw-repmat(T,1,3));
-Ac1=Rw2c*(Aw)+repmat(T,1,3);
+Ac1=R*Rc2w*(Aw-repmat(T,1,3));
 
-%
-Ac1=R*Ac1/10;
 plot3(Ac1(1,1),Ac1(2,1),Ac1(3,1),'or','MarkerFaceColor','r','MarkerEdgeColor','r','MarkerSize',5);
 plot3(Ac1(1,2),Ac1(2,2),Ac1(3,2),'og','MarkerFaceColor','g','MarkerEdgeColor','g','MarkerSize',5);
 plot3(Ac1(1,3),Ac1(2,3),Ac1(3,3),'ob','MarkerFaceColor','b','MarkerEdgeColor','b','MarkerSize',5);
@@ -223,17 +236,30 @@ line(Prj(1,:),Prj(2,:),Prj(3,:),'Color','g','LineStyle','-')
 Prj=[Ow Ac1(:,3)];
 line(Prj(1,:),Prj(2,:),Prj(3,:),'Color','b','LineStyle','-')
  
-Zw1=Rw2c*Zw+T;
-Zw1=R*Zw1;
+Zw1=R*Rc2w*(Zw-T);
 %Disegno il punto Zw
 plot3(Zw1(1),Zw1(2),Zw1(3),'ob','MarkerFaceColor','b','MarkerEdgeColor','b','MarkerSize',5);
 title('CAMERA REFERENCE SYSTEM')
 Prj=[Co1 Zw1];
 line(Prj(1,:),Prj(2,:),Prj(3,:),'Color','r','LineStyle','-')
 
+figure(3);hold on
+plot(P2D(1),P2D(2),'og','MarkerFaceColor','g','MarkerEdgeColor','g','MarkerSize',5);
+[x,y,z] = sphere(30);
+figure(2)
+[m,n]=size(x);
+x=reshape(x,1,m*n);
+y=reshape(y,1,m*n);
+z=reshape(z,1,m*n);
+P=10*[x;y;z]+10
+P1=R*Rc2w*(P-repmat(T,1,m*n));
+x=reshape(P1(1,:),m,n);
+y=reshape(P1(2,:),m,n);
+z=reshape(P1(3,:),m,n);
+surf(x,y,z)  % sphere centered at origin
+
 figure(3)
-%Calcolo la proiezione di Zw sul piano immagine:
-Pixel=K*(Rw2c*Zw+T)
-Pixel=Pixel/Pixel(3);
-plot3(Pixel(1),Pixel(2),f,'og','MarkerFaceColor','g','MarkerEdgeColor','g','MarkerSize',5);
- 
+P=K*(Rw2c*P+repmat(T,1,m*n));
+u=P(1,:)./P(3,:);
+v=P(2,:)./P(3,:);
+plot(u,v,'ob','MarkerFaceColor','b','MarkerEdgeColor','b','MarkerSize',5)
